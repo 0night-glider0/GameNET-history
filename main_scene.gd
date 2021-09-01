a7721551ee2e7d23b88e2ce2b4b5f02c326134cd
0night-glider0 <nightgliderdev@gmail.com>
2021-09-01 14:41:50 +0300

    Новые надписи

--- main_scene.gd
+++ main_scene.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_Button_pressed():
 	OS.shell_open("https://gamejolt.com/games/lost_signal/635244")
+
+
+func _on_Button2_pressed():
+	OS.shell_open("https://forms.gle/zzWBmATFap6AXxUR9")
