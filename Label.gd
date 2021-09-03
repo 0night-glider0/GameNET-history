0a1546e4db3eb70698e08f798c65a504fc17c7df
0night-glider0 <nightgliderdev@gmail.com>
2021-09-03 21:09:05 +0300

    Обновление музея. Мобильная версия с уменьшенным зумом

--- Label.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-export (String, MULTILINE) var mobile_text = "text_mobile"
-
-func _ready():
-	if OS.has_touchscreen_ui_hint():
-		text = mobile_text
--- main_scene.gd
+++ main_scene.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+func _ready():
+	if OS.has_touchscreen_ui_hint():
+		get_node("YSort/player/Camera2D").zoom = Vector2(0.4, 0.4)
+		get_node("text/Label").text = "Используйте палец\nдля передвижения"
+		get_node("text/Label2").text = "Нажмите два раза пальцем\nна коробку и перетащите"
+
+
 func _on_Button_pressed():
 	OS.shell_open("https://gamejolt.com/games/lost_signal/635244")
 
